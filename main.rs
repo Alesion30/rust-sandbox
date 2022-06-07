0bcb19916210a1bc266aaaff19fed916562b8236
Yuta Ohira <alesion90@gmail.com>
2022-06-07 17:55:52 +0900

    add: futures::executorを用いた非同期処理

--- main.rs
+++ main.rs
@@ -1,3 +1,16 @@
+use futures::executor;
+
 fn main() {
-  println!("Hello World");
+    executor::block_on(async_add_logger(2, 8));
+}
+
+/** 非同期足し算 */
+async fn async_add(left: i32, right: i32) -> i32 {
+    return left + right;
+}
+
+/** 非同期足し算 ログを出力 */
+async fn async_add_logger(left: i32, right: i32) {
+    let result = async_add(left, right).await;
+    println!("{} + {} = {}", left, right, result);
 }
